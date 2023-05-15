b945c6e749f26f6f56adf9f5ca964399bf6311d1
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-05-15 15:26:26 +0300

    Convert string input to Morse Code

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 """
 Program that takes any String input and converts it into Morse Code
-– represents dahs
-· represents dits
+-	Dash (dahs)
+.	Dot (dits)
+Space	Letter separator
+/	Word separator
+#	Untranslatable character
 """
+
+from morse_code import *
+
+string_to_convert = input("Your input: ")
+output = ""
+
+for element in string_to_convert:
+    if element == " ":
+        code = "/"
+    else:
+        try:
+            code = morse_code[element.upper()]
+        except KeyError:
+            code = "#"
+    output += code
+    output += " "
+
+print(f"In Morse's code:\n{output}")
